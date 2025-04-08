67876e0c2f62b907cd2504aab10f5b26c0c87818
Santiago <santisch17@gmail.com>
2025-04-08 01:21:22 +0000

    Cree el .c

---
+++ 00-HelloWorld/HelloWorld.c
@@ -0,0 +1,13 @@
+/*
+Este es el TP Hola mundo
+Autor: Santiago Schneideroff
+Ultima modificacion: 22:02
+*/
+#include <stdio.h>
+
+int main(void){
+
+    printf("Hello world!!");
+
+    return 0;
+}
